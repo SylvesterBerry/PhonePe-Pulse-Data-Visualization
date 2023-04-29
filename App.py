e2529bcbd6221845c2e7e266b0d533446cbdf8e5
SylvesterBerry <119794300+SylvesterBerry@users.noreply.github.com>
2023-04-29 10:11:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -483,7 +483,7 @@ with tab3:
       #             We Can see that Registered_Users and App Openings tend to increase every year""")
         
 #st.write('## :pink[Top 3 States Data]')
-c1,c2 = st.columns(2)
+#c1,c2 = st.columns(2)
 with c1:
     Year = st.selectbox(
             'Select the Year',
